2ee4ec4ff00ef408d80dbb2940e5de0ee394ccb6
RahulMuk <r.mukherjee5@icloud.com>
2015-07-18 23:16:10 -0400

    Initial push to upload graphics

---
+++ drugarrests/DrugGraphic_edgeActions.js
@@ -0,0 +1,248 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Wilson}","mouseover",function(sym,e){sym.stop(250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Wilson}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Perkins}","mouseenter",function(sym,e){sym.stop(500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Perkins}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Washington}","mouseenter",function(sym,e){sym.stop(1000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Washington}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Glidden}","mouseenter",function(sym,e){sym.stop(1250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Glidden}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Convo}","mouseenter",function(sym,e){sym.stop(1500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Convo}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bike_Path}","mouseenter",function(sym,e){sym.stop(1750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bike_Path}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_South_Green}","mouseenter",function(sym,e){sym.stop(2000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_South_Green}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Milliron}","mouseenter",function(sym,e){sym.stop(2250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Milliron}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_CollegeGreen}","mouseenter",function(sym,e){sym.stop(2500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_CollegeGreen}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_HighStreet}","mouseenter",function(sym,e){sym.stop(2750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_HighStreet}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_VoigtHall}","mouseenter",function(sym,e){sym.stop(3000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_VoigtHall}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_UnionSt}","mouseenter",function(sym,e){sym.stop(3250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_UnionSt}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Putnam}","mouseenter",function(sym,e){sym.stop(3500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Putnam}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bryant}","mouseenter",function(sym,e){sym.stop(3750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bryant}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Lincoln}","mouseenter",function(sym,e){sym.stop(4000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Lincoln}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_StewartSt}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_StewartSt}","mouseenter",function(sym,e){sym.stop(4250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_OUPD}","mouseenter",function(sym,e){sym.stop(4500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_OUPD}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_PoplarSt}","mouseenter",function(sym,e){sym.stop(4750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_PoplarSt}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Oxbow_Trail}","mouseenter",function(sym,e){sym.stop(5000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Oxbow_Trail}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Oblennes}","mouseenter",function(sym,e){sym.stop(5250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Oblennes}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Atkinson}","mouseenter",function(sym,e){sym.stop(5500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Atkinson}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_True}","mouseenter",function(sym,e){sym.stop(5750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_True}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Dougan}","mouseenter",function(sym,e){sym.stop(6000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Dougan}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ewing}","mouseenter",function(sym,e){sym.stop(6250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ewing}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Adams}","mouseenter",function(sym,e){sym.stop(6500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Adams}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ping}","mouseenter",function(sym,e){sym.stop(6750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ping}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Tailgreat_Park}","mouseenter",function(sym,e){sym.stop(7000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Tailgreat_Park}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_The_Ridges}","mouseenter",function(sym,e){sym.stop(7250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_The_Ridges}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_WaterTowerDr}","mouseenter",function(sym,e){sym.stop(7500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_WaterTowerDr}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Treudly}","mouseenter",function(sym,e){sym.stop(7750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Treudly}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_GolfCourse}","mouseenter",function(sym,e){sym.stop(8000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_GolfCourse}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Crawford}","mouseenter",function(sym,e){sym.stop(8250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Crawford}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Brough}","mouseenter",function(sym,e){sym.stop(8500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Brough}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Brown}","mouseenter",function(sym,e){sym.stop(8750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Brown}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Mackinnon}","mouseenter",function(sym,e){sym.stop(9000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Mackinnon}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Foster}","mouseenter",function(sym,e){sym.stop(9250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Foster}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Biddle}","mouseenter",function(sym,e){sym.stop(9500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Biddle}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Fenzel}","mouseenter",function(sym,e){sym.stop(9750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Fenzel}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Marzoff}","mouseenter",function(sym,e){sym.stop(10000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Marzoff}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Armbruster}","mouseenter",function(sym,e){sym.stop(10250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Armbruster}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bromley}","mouseenter",function(sym,e){sym.stop(10500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bromley}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ryors}","mouseenter",function(sym,e){sym.stop(10750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Ryors}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_SargentHall}","mouseenter",function(sym,e){sym.stop(11000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_SargentHall}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_JamesHall}","mouseenter",function(sym,e){sym.stop(11250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_JamesHall}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Boyd}","mouseenter",function(sym,e){sym.stop(11500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Boyd}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Hoover}","mouseenter",function(sym,e){sym.stop(11750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Hoover}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Wray}","mouseenter",function(sym,e){sym.stop(12000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Wray}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Weld}","mouseenter",function(sym,e){sym.stop(12250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Weld}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Tiffin}","mouseenter",function(sym,e){sym.stop(12500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Tiffin}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Gam}","mouseenter",function(sym,e){sym.stop(12750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Gam}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bush}","mouseenter",function(sym,e){sym.stop(13000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bush}","mouseleave",function(sym,e){sym.stop(13000);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Read}","mouseenter",function(sym,e){sym.stop(13250);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Read}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ScottQd}","mouseenter",function(sym,e){sym.stop(13500);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ScottQd}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Clip}","mouseenter",function(sym,e){sym.stop(13750);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Clip}","mouseleave",function(sym,e){sym.stop(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_WashingtonCopy}","mouseenter",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_WashingtonCopy}","mouseleave",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bike_PathCopy}","mouseenter",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Bike_PathCopy}","mouseleave",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_BroughCopy}","mouseenter",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_BroughCopy}","mouseleave",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_MackinnonCopy}","mouseenter",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_MackinnonCopy}","mouseleave",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ClipCopy}","mouseenter",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ClipCopy}","mouseleave",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_OblennesCopy}","mouseenter",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_OblennesCopy}","mouseleave",function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-84479960");

0f183fc7c178ee2331d8ef45377f32080df815b3
hdebenham <admin@dhcp-205-187.cns.ohiou.edu>
2017-02-02 21:19:57 -0500

    testing reba's love story

--- Working-SpecialProject/ohio-love-story/js/reba.js
+++ Working-SpecialProject/ohio-love-story/js/reba.js
@@ -1,8 +1,19 @@
 
 $(document).ready(function () {
     "use strict";
-    $('.brick_row').click(function () {
-        $('.love_story').show();
+    $('#brick1').click(function () {
+        $('#love_story1').show('fast');
+    });
     
+    $('#love_story1').click(function () {
+        $('#love_story1').remove();
+    });
+    
+    $('#brick2').click(function () {
+        $('#love_story2').show('fast');
+    });
+    
+    $('#love_story2').click(function () {
+        $('#love_story2').remove();
     });
 });

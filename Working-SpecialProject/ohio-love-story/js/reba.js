b5c4dfa79e7fdb00a92c6493163114396c9c2c3a
hdebenham <admin@dhcp-205-187.cns.ohiou.edu>
2017-02-09 01:12:54 -0500

    finally in!!!

--- Working-SpecialProject/ohio-love-story/js/reba.js
+++ Working-SpecialProject/ohio-love-story/js/reba.js
@@ -5,15 +5,87 @@ $(document).ready(function () {
         $('#1').show('fast');
     });
     
-    $('#1').click(function () {
-        $('#1').hide();
-    });
-    
     $(document).on('click', '#brick2', function () {
         $('#2').show('fast');
     });
+       
+    $(document).on('click', '#brick3', function () {
+        $('#3').show('fast');
+    });
     
-    $('#2').click(function () {
-        $('#2').hide();
+    $(document).on('click', '#brick4', function () {
+        $('#4').show('fast');
+    });
+    
+    
+    $(document).on('click', '#brick5', function () {
+        $('#5').show('fast');
+    });
+    
+    
+    $(document).on('click', '#brick6', function () {
+        $('#6').show('fast');
+    });
+    
+    
+    $(document).on('click', '#brick7', function () {
+        $('#7').show('fast');
+    });
+    
+    
+    $(document).on('click', '#brick8', function () {
+        $('#8').show('fast');
+    });
+
+    
+    $(document).on('click', '#brick9', function () {
+        $('#9').show('fast');
+    });
+
+    
+    $(document).on('click', '#brick10', function () {
+        $('#10').show('fast');
+    });
+    
+    $(document).on('click', '#brick11', function () {
+        $('#11').show('fast');
+    });
+    $(document).on('click', '#brick12', function () {
+        $('#12').show('fast');
+    });
+    $(document).on('click', '#brick13', function () {
+        $('#13').show('fast');
+    });
+    $(document).on('click', '#brick14', function () {
+        $('#14').show('fast');
+    });
+    $(document).on('click', '#brick15', function () {
+        $('#15').show('fast');
+    });
+    $(document).on('click', '#brick16', function () {
+        $('#16').show('fast');
+    });
+    $(document).on('click', '#brick17', function () {
+        $('#17').show('fast');
+    });
+    $(document).on('click', '#brick18', function () {
+        $('#18').show('fast');
+    });
+    $(document).on('click', '#brick19', function () {
+        $('#19').show('fast');
+    });
+    $(document).on('click', '#brick20', function () {
+        $('#20').show('fast');
+    });
+    $(document).on('click', '#brick21', function () {
+        $('#21').show('fast');
+    });
+    $(document).on('click', '#brick22', function () {
+        $('#22').show('fast');
+    });
+    
+    
+    $('.love_story').click(function () {
+        $('.love_story').hide();
     });
 });

e6c4645257efcf13f933839a96c1b1c16a298097
Taylor Johnston <tj369915@ohio.edu>
2019-07-29 23:36:17 -0500

    the beginnings of the recruitment page

---
+++ JoinUs/js/main.js
@@ -0,0 +1,15 @@
+
+
+// STICKY NAV
+var stickyEl = document.querySelector('.me_sticky')
+var stickyPosition = stickyEl.getBoundingClientRect().top;
+var offset = -20
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset >= stickyPosition + offset) {
+      stickyEl.style.position = 'fixed';
+      stickyEl.style.top = '0px';
+  } else {
+      stickyEl.style.position = 'static';
+      stickyEl.style.top = '';
+  }
+});

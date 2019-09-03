cdb49c3a5b754e23f5b3ae33177dd45cef2df4ab
Taylor Johnston <tj369915@ohio.edu>
2019-09-03 11:28:47 -0400

    crimes

--- Working-SpecialProject/ohio-university-athens-crimes-2019-2020/js/main.js
+++ Working-SpecialProject/ohio-university-athens-crimes-2019-2020/js/main.js
@@ -701,50 +701,38 @@ map.on('load', () => {
             ]
         }
     });
-
-    map.addLayer({
-        id: 'points',
-        source: 'pointsSource',
-        type: 'circle',
-        paint: {
-            'circle-radius': 5,
-            'circle-color': '#329ce8',
-            'circle-opacity': 0.85,
-            'circle-stroke-color': '#FFFFFF',
-            'circle-stroke-width': 1
-        }
-    });
-
-
-    // When a click event occurs on a feature in the places layer, open a popup at the
-    // location of the feature, with description HTML from its properties.
-    map.on('click', 'points', function(e) {
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var description = e.features[0].properties.copy;
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(title)
-            .addTo(map);
-    });
-
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'points', function() {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'points', function() {
-        map.getCanvas().style.cursor = '';
-    });
 });
+
+map.addLayer({
+    id: 'points',
+    source: 'pointsSource',
+    type: 'circle',
+    paint: {
+        'circle-radius': 5,
+        'circle-color': '#329ce8',
+        'circle-opacity': 0.85,
+        'circle-stroke-color': '#FFFFFF',
+        'circle-stroke-width': 1
+    }
+});
+
+map.on('click', 'points', function(e) {
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    //        var description = e.features[0].properties.description;
+    console.log(e);
+    var lat = e.features[0].properties['Latitude'];
+    var lng = e.features[0].properties['Longitude'];
+    var crimes = '<p class="pop">' + e.features[0].properties['date'];
+    crimes += '<br>' + e.features[0].properties['title'];
+    crimes += '<br>' + e.features[0].properties['copy'];
+
+
+    new mapboxgl.Popup()
+        .setLngLat([Longitude, Latitude])
+        .setHTML(crimes)
+        .addTo(map);
+});
+
 
 map.scrollZoom.disable(); // disable scroll zoom
 map.addControl(new mapboxgl.NavigationControl()); // add zoom/nav controls

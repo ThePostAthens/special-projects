2fd28396ccc474ef3377e6677af20db7a1198449
Taylor Johnston <tj369915@ohio.edu>
2020-03-11 21:34:58 -0400

    map is done for now

--- SpecialProjects/ohio-university-covid-19-coverage/js/main.js
+++ SpecialProjects/ohio-university-covid-19-coverage/js/main.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obnN0b250MDUiLCJhIjoiY2pkeG96ajVoNG5wZzJ3cDBnMHJtdmUwMiJ9.aMosLr-Y_82V50tp2mgpYw';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v10', // stylesheet location
+    style: 'mapbox://styles/johnstont05/ck7o27avw1t511jqrkwe48lxs', // stylesheet location
     center: [-82.849, 40.239],
     zoom: 6.4
 
@@ -18,9 +18,17 @@ map.on('load', function() {
         'type': 'fill',
         'source': 'counties',
         'paint': {
-            'fill-color': 'rgba(200, 100, 240, 0.4)',
-            'fill-outline-color': 'black'
-        }
+      'fill-color': [
+        'interpolate',
+        ['linear'],
+        ['to-number', ['get', 'CASES']],
+        0, '#ffeda0',
+        2, '#feb24c',
+        5, '#f03b20',
+      ],
+      'fill-opacity': 0.5,
+      'fill-outline-color': 'black'
+    }
     });
 
     // When a click event occurs on a feature in the countiess layer, open a popup at the

100ba86ef57747e68a3ff91fab8fa7cf4a01d735
bllender00 <bllender.00@gmail.com>
2021-01-14 07:03:21 -0500

    A-B done

--- SpecialProjects/layoff-guide-table-testing/js/main.js
+++ SpecialProjects/layoff-guide-table-testing/js/main.js
@@ -1,36 +1,16 @@
-var dataSet = [
-  ["Boals, Jeffrey Donald","MENS BASKETBALL","Head Men's Basketball Coach","N/A","$581,000.00"  ],
-  ["Solich, Frank","FOOTBALL","Head Football Coach","N/A","$524,928.00"  ],
-  ["Nellis, Marvin Duane (M.Duane)","PRESIDENT","President","Professor","$489,357.00"  ],
-  ["Djalali, Chaden","PROVOST","Executive Vice President and Provost","N/A","$378,750.00"  ],
-  
-  
-];
-
 $(document).ready(function() {
-    $('#example').DataTable({
-        data: dataSet,
-        "scrollX": true,
-        "order": [[ 4, "desc" ]],
-        columns: [{
-                title: "Name"
-            },
-            {
-                title: "Title"
-            },
-            {
-                title: "Department"
-            },
-            {
-                title: "Salary"
-            }
-        ],
-        language: {
-            searchPlaceholder: "Search records",
-            search: "",
-        },
-        "dom": '<"pull-left"f><"pull-right"l>tip'
-
-
-    });
-});
+    var t = $('#example').DataTable( {
+        "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+        "order": [[ 1, 'asc' ]]
+    } );
+ 
+    t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
+} );
